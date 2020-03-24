3533b8aef079f0f66a2b183b1bfff05d06aa984c
Bonifacio Aguilar Jr <aguilar.beej7@gmail.com>
2020-03-24 18:27:26 +0800

    billability report new approach

--- AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/ReportsCommands/generateemployeebillabilityreport.bat
+++ AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/ReportsCommands/generateemployeebillabilityreport.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files (x86)\kool-aide\kool-aide" generate-report -r employee-billability --format excel --output "C:\GeneratedReports\Retail Services Employee Billability.xlsx" --params {\"sorts\":[\"EmployeeName\"],\"departments\":[1],\"divisions\":[1],\"fys\":["%1"]}
+"C:\Program Files (x86)\kool-aide\kool-aide" generate-report -r employee-billability --format excel --output "C:\GeneratedReports\Retail Services Employee Billability.xlsx" --params {\"sorts\":[\"EmployeeName\"],\"departments\":[1],\"divisions\":[1],\"fys\":[\"%1\"]}
