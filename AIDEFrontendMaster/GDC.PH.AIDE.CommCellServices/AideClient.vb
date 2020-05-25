d055e4528a33aeaf8dfa180610781cc80fcd78e1
chriscg <chriscguevara2012@gmail.com>
2020-05-25 18:53:21 +0800

    Handle exception in AideClient, https://github.com/rsx-labs/aide-frontend/issues/579

--- AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/AideClient.vb
+++ AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/AideClient.vb
@@ -1,11 +1,19 @@
-﻿Imports System.ServiceModel
+﻿Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+Imports System.ServiceModel
 Imports GDC.PH.AIDE.ServiceFactory
 Imports UI_AIDE_CommCellServices.ServiceReference1
 
 Public Class AideClient
+    Implements INotifyPropertyChanged
+
     Private Shared _context As InstanceContext
     Private Shared _instance As AideClient
     Private Shared _aideClientService As AideServiceClient
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+    ''Public Shared Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+    'Private Event INotifyPropertyChanged_PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
     Private Sub New()
         CreateClient()
     End Sub
@@ -18,11 +26,22 @@ Public Class AideClient
     End Sub
 
     Public Shared Function GetClient() As AideServiceClient
+        If _aideClientService.State <> CommunicationState.Opened Then
+            CreateClient()
+        End If
         Return _aideClientService
     End Function
 
+
     Private Shared Sub CreateClient()
-        Dim factory As ServiceClientFactory = ServiceClientFactory.GetFactory()
-        _aideClientService = factory.GetClient(Of IAideService)(_context)
+        Try
+            Dim factory As ServiceClientFactory = ServiceClientFactory.GetFactory()
+            _aideClientService = factory.GetClient(Of IAideService)(_context)
+            _aideClientService.Open()
+        Catch ex As Exception
+            _aideClientService = New AideServiceClient(_context)
+            _aideClientService.Open()
+        End Try
+
     End Sub
 End Class
