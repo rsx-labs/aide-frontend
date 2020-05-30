5572be1970a4b8c22d915bb359c30be253f691d3
Giann Carlo Camilo <gianncarlo.camilo1995@yahoo.com>
2020-05-31 05:17:40 +0800

    Workplace Audit - Able to select schedule if you were the assigned employee, - Fixed bug when pressing others schedule and selecting different day/month/week/ will display blank schedule. - Fixed remarks & Status when saving workplace audit.

--- AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/View/MenuPage/Workplace Audit/DailyAuditPage.xaml.vb
+++ AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/View/MenuPage/Workplace Audit/DailyAuditPage.xaml.vb
@@ -338,23 +338,24 @@ Class DailyAuditPage
     End Sub
 
     Private Sub ListViewItem_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs)
-        Dim item = (TryCast(sender, FrameworkElement)).DataContext
-
-        Dim FYDBProvider As New WorkplaceAuditDBProvider
-
-        For Each objFiscal As WorkplaceAudit In lstAuditQuestions
-            If objFiscal.WEEKDATE.ToString.Trim() = item.WEEKDATE.ToString.Trim() Then
-                FYDBProvider.SetMyWorkplaceAudit(objFiscal)
-            End If
-
-        Next
-
-        LstAuditDailySchedByWeek.Clear()
-        For Each rawUser As MyWorkplaceAudit In FYDBProvider.GetMyWorkplaceAudit()
-            LstAuditDailySchedByWeek.Add(New WorkplaceAuditModel(rawUser))
-        Next
-
         If profile.Emp_ID = currDailyAuditAssigned OrElse profile.Permission_ID = 1 Then
+            Dim item = (TryCast(sender, FrameworkElement)).DataContext
+
+            Dim FYDBProvider As New WorkplaceAuditDBProvider
+
+            For Each objFiscal As WorkplaceAudit In lstAuditQuestions
+                If objFiscal.WEEKDATE.ToString.Trim() = item.WEEKDATE.ToString.Trim() Then
+                    FYDBProvider.SetMyWorkplaceAudit(objFiscal)
+                End If
+
+            Next
+
+            LstAuditDailySchedByWeek.Clear()
+            For Each rawUser As MyWorkplaceAudit In FYDBProvider.GetMyWorkplaceAudit()
+                LstAuditDailySchedByWeek.Add(New WorkplaceAuditModel(rawUser))
+            Next
+
+
             addframe.Navigate(New DailyAuditCheck(pageframe, profile, addframe, menugrid, submenuframe, LstAuditDailySchedByWeek, 1))
             pageframe.IsEnabled = False
             pageframe.Opacity = 0.3
@@ -414,7 +415,7 @@ Class DailyAuditPage
                     Else
                         imgdtcheck = "..\..\..\Assets\Button\wrong.png"
                     End If
-
+                    currDailyAuditAssigned = quest.EMP_ID
                     dailyVMM.QuestionDayList.Add(New QuestionsDayModel(quest.AUDIT_QUESTIONS, quest.OWNER, quest.DT_CHECK_FLG, imgdtcheck, quest.WEEKDATE))
                 End If
 
