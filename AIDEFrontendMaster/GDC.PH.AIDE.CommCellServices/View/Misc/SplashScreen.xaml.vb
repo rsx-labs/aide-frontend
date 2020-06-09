ae5356f770c6ab744a9ad6b52425f2cff10f15ee
trashvin <m3lles@gmail.com>
2020-06-09 16:39:20 +0800

    Refactoring

--- AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/View/Misc/SplashScreen.xaml.vb
+++ AIDEFrontendMaster/GDC.PH.AIDE.CommCellServices/View/Misc/SplashScreen.xaml.vb
@@ -19,38 +19,38 @@ Public Class SplashScreen
         Select Case DateTime.Now.Hour
             Case <= 4
                 data.Message = "Good morning,"
-                data.ImageSource = ".\Assets\Images\night.png"
-                data.Background = "#616161"
+                data.ImageSource = "..\..\Assets\Images\night.png"
+                data.Background = "#1a237e"
                 data.Foreground = "#fafafa"
             Case <= 7
                 data.Message = "Good morning,"
-                data.ImageSource = ".\Assets\Images\sunrise.png"
-                data.Background = "#f44336"
+                data.ImageSource = "..\..\Assets\Images\sunrise.png"
+                data.Background = "#5c6bc0"
                 data.Foreground = "#fafafa"
             Case <= 11
                 data.Message = "Good morning,"
-                data.ImageSource = ".\Assets\Images\morning.png"
-                data.Background = "#ffebee"
+                data.ImageSource = "..\..\Assets\Images\morning.png"
+                data.Background = "#9fa8da"
                 data.Foreground = "#4e342e"
             Case <= 14
                 data.Message = "Good afternoon,"
-                data.ImageSource = ".\Assets\Images\noon.png"
-                data.Background = "#ffebee"
+                data.ImageSource = "..\..\Assets\Images\noon.png"
+                data.Background = "#e8eaf6"
                 data.Foreground = "#4e342e"
             Case <= 17
                 data.Message = "Good afternoon,"
-                data.ImageSource = ".\Assets\Images\afternoon.png"
-                data.Background = "#f44336"
+                data.ImageSource = "..\..\Assets\Images\afternoon.png"
+                data.Background = "#9fa8da"
                 data.Foreground = "#fafafa"
             Case <= 20
                 data.Message = "Good evening,"
-                data.ImageSource = ".\Assets\Images\evening.png"
-                data.Background = "#b71c1c"
+                data.ImageSource = "..\..\Assets\Images\evening.png"
+                data.Background = "#5c6bc0"
                 data.Foreground = "#fafafa"
             Case Else
                 data.Message = "Good evening,"
-                data.ImageSource = ".\Assets\Images\night.png"
-                data.Background = "#616161"
+                data.ImageSource = "..\..\Assets\Images\night.png"
+                data.Background = "#1a237e"
                 data.Foreground = "#fafafa"
         End Select
 
